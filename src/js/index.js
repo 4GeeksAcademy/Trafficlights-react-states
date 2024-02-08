ddb77c8682f3b32893b625125a0a750912d13b26
Lucía Pacheco Daldés <150815101+Emerydith@users.noreply.github.com>
2024-02-08 19:15:49 +0000

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 //import react into the bundle
-import React from "react";
 import ReactDOM from "react-dom";
+import React, { useState } from 'react';
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
@@ -8,5 +8,34 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
+const TrafficLight = () => {
+    const [activeLight, setActiveLight] = useState(null);
+  
+    const handleClick = (color) => {
+      setActiveLight(color);
+    };
+  
+    return (
+        <><div className="cable"></div>
+        <div className="traffic-light">
+            <div
+                className={`light red ${activeLight === 'red' ? 'active' : ''}`}
+                onClick={() => handleClick('red')}
+            ></div>
+            <div
+                className={`light yellow ${activeLight === 'yellow' ? 'active' : ''}`}
+                onClick={() => handleClick('yellow')}
+            ></div>
+            <div
+                className={`light green ${activeLight === 'green' ? 'active' : ''}`}
+                onClick={() => handleClick('green')}
+            ></div>
+        </div></>
+    );
+  };
+  
+
+export default TrafficLight;
+
+
 ReactDOM.render(<Home />, document.querySelector("#app"));
